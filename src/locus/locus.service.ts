7d27aea8334d06d8913c54047f785eb6f44e4f19
to4ka <toka7076@gmail.com>
2026-04-30 18:58:28 +0000

    Added ordering for memberCount, locusStart and locusStop

--- src/locus/locus.service.ts
+++ src/locus/locus.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { Prisma, RncLocus, RncLocusMember } from 'src/generated/prisma/client';
-import { GetLocusQuery, SideloadingOption } from './dto/get-request.dto';
+import { GetLocusQuery, SideloadingOption, OrderDir } from './dto/get-request.dto';
 
 
 export type LocusWithMembers = RncLocus & {
@@ -26,28 +26,22 @@ export class LocusService {
     const skip = query.index ?? 0;
 
     const where: Prisma.RncLocusWhereInput = {
-      ...(query.id && { id: { in: query.id.map(BigInt) } }),
-      ...(query.assemblyId && { assemblyId: query.assemblyId }),
-      ...(query.regionId && {
-        members: {
-          some: {
-            regionId: { in: query.regionId },
-          },
-        },
-      }),
-      ...(query.membershipStatus && {
-        members: {
-          some: {
-            membershipStatus: query.membershipStatus,
-          },
-        },
-      }),
+      ...(query.id               && { id: { in: query.id.map(BigInt) } }),
+      ...(query.assemblyId       && { assemblyId: query.assemblyId }),
+      ...(query.regionId         && { members: { some: { regionId: { in: query.regionId } } } }),
+      ...(query.membershipStatus && { members: { some: { membershipStatus: query.membershipStatus } } }),
     };
+
+    const orderBy: Prisma.RncLocusOrderByWithRelationInput | undefined =
+      query.order
+        ? { [query.order]: query.orderDir ?? OrderDir.ASC }
+        : undefined;
 
     const findManyArgs: Prisma.RncLocusFindManyArgs = {
       take,
       skip,
       where,
+      orderBy,
       include: {
         members: includeMembers,
       },
