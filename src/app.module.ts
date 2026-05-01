99d994468b49e9862de9a5bc8c0ee6802937ac1a
to4ka <toka7076@gmail.com>
2026-05-01 05:22:33 +0000

    Added users service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { LocusModule } from './locus/locus.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [LocusModule, PrismaModule],
+  imports: [LocusModule, PrismaModule, UsersModule],
   controllers: [],
   providers: [],
 })
